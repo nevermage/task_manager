42db1654c4fc3f5a303477461e15f400112d92de
bihunov <maksym.bihunov@nixs.com>
2024-06-08 19:21:54 +0300

    add redirect from '/' route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (!auth()->user()) {
+        return redirect('/login');
+    }
+
+    return redirect('/board');
 });
 
 Route::get('/board', [Controllers\Board\BoardController::class, 'index'])
