9f1a604f66639b49dec43e88611d73920c277785
bihunov <maksym.bihunov@nixs.com>
2024-06-10 09:05:20 +0300

    update board redirect

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if (!auth()->user()) {
-        return redirect('/login');
+        return redirect('/register');
     }
 
     return redirect('/board');
